ae1729aaa67bebb07868efd1e99add58255da5bd
unknown <aruzhan.bazarbayeva@nu.edu.kz>
2022-10-23 01:14:57 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,25 @@
+
+//question 2
+let length = 2
+let width = 4 
+let perimeter = 2*(length+width)
+console.log (length, width, perimeter)
+
+
+//question 9
+let sign = -1
+
+if (sign < 0) 
+    console.log("negative");
+else if (sign == 0)
+    console.log ("neither negative nor positive");
+else
+console.log ("positive")
+  
+//question 12
+let number = 0.5
+
+if (number > 0 && number < 1) 
+    console.log("between 0 and 1");
+else
+console.log ("not between 0 and 1")
